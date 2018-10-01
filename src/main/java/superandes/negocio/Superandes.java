ebb6466b05efe8f70a26aa63e39a84e93beb575b
sacostav <s.acostav@uniandes.edu.co>
2018-10-01 16:35:13 -0500

    Commit

--- src/main/java/superandes/negocio/Superandes.java
+++ src/main/java/superandes/negocio/Superandes.java
@@ -1,5 +1,67 @@
 package superandes.negocio;
 
+import java.util.logging.Logger;
+
+import com.google.gson.JsonObject;
+
+import superandes.persistencia.PersistenciaSuperandes;
+
 public class Superandes {
 
+
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Logger para escribir la traza de la ejecución
+	 */
+	private static Logger log = Logger.getLogger(Superandes.class.getName());
+	
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia
+	 */
+	private PersistenciaSuperandes pp;
+	
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	/**
+	 * El constructor por defecto
+	 */
+	public Superandes ()
+	{
+		pp = PersistenciaSuperandes.getInstance ();
+	}
+	
+	/**
+	 * El constructor qye recibe los nombres de las tablas en tableConfig
+	 * @param tableConfig - Objeto Json con los nombres de las tablas y de la unidad de persistencia
+	 */
+	public Superandes (JsonObject tableConfig)
+	{
+		pp = PersistenciaSuperandes.getInstance (tableConfig);
+	}
+	
+	/**
+	 * Cierra la conexión con la base de datos (Unidad de persistencia)
+	 */
+	public void cerrarUnidadPersistencia ()
+	{
+		pp.cerrarUnidadPersistencia ();
+	}
+	
+	/* *************************************************
+	 * Metodos para manejar las sucursales
+	 ***************************************************/
+	
+	/**
+	 * Adiciona de manera persistente una sucursal 
+	 * Adiciona entradas al log de la aplicación
+	 * @param nombre - El nombre del tipo de bebida
+	 * @return El objeto TipoBebida adicionado. null si ocurre alguna Excepción
+	 */
+	
 }
---
+++ src/main/java/superandes/persistencia/PersistenciaSuperandes.java
@@ -0,0 +1,21 @@
+package superandes.persistencia;
+
+import com.google.gson.JsonObject;
+
+public class PersistenciaSuperandes {
+
+	public static  PersistenciaSuperandes getInstance() {
+		return null;
+	}
+
+	public static PersistenciaSuperandes getInstance(JsonObject tableConfig) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void cerrarUnidadPersistencia() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
