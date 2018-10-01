c777373381c2c21865a8eaa1ad620bb02cf6f471
sacostav <s.acostav@uniandes.edu.co>
2018-10-01 18:31:28 -0500

    SQL

--- src/main/java/superandes/persistencia/PersistenciaSuperandes.java
+++ src/main/java/superandes/persistencia/PersistenciaSuperandes.java
@@ -4,8 +4,11 @@ import org.apache.log4j.Logger;
 
 import com.google.gson.JsonObject;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.sql.Timestamp;
+
+import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
 
@@ -29,11 +32,67 @@ public class PersistenciaSuperandes {
 	
 	private PersistenceManagerFactory pmf;
 	
-	private List<String> tablas;
+	private LinkedList<String> tablas;
 	
 	private SQLUtil sqlUtil;
 	
-	public static  PersistenciaSuperandes getInstance() {
+	private SQLBodega sqlBodega;
+	
+	private SQLCliente sqlCliente;
+	
+	private SQLEstante sqlEstante;
+	
+	private SQLProducto sqlProducto;
+	
+	private SQLProveedor sqlProveedor;
+	
+	private SQLSucursal sqlSucursal;
+	
+	private SQLSupermercado sqlSupermercado;
+	
+	private SQLCategoriaProducto sqlCategoriaProducto;
+	
+	
+	/* ****************************************************************
+	 * 			Métodos del MANEJADOR DE PERSISTENCIA
+	 *****************************************************************/
+
+	/**
+	 * Constructor privado con valores por defecto - Patrón SINGLETON
+	 */
+	
+	public PersistenciaSuperandes( ) {
+		
+		tablas = new LinkedList<String>();
+		tablas.add("Superandes_sequence");
+		tablas.add("BODEGA");
+		tablas.add("CLIENTE");
+		tablas.add("ESTANTE");
+		tablas.add("PRODUCTO");
+		tablas.add("CATEGORIAPRODUCTO");
+		tablas.add("PROVEEDOR");
+		tablas.add("SUCURSAL");
+		tablas.add("SUPERMERCADO");
+		
+		
+	}
+	
+
+	private PersistenciaSuperandes(JsonObject tableConfig)
+	{
+		crearClasesSQL();
+		tablas = leerNombreTablas(tableConfig);
+		
+
+		String unidadPersistencia = tableConfig.get ("unidadPersistencia").getAsString ();
+		log.trace ("Accediendo unidad de persistencia: " + unidadPersistencia);
+		pmf = JDOHelper.getPersistenceManagerFactory (unidadPersistencia);
+	
+	}
+		
+
+		public static  PersistenciaSuperandes getInstance() {
+	
 		return null;
 	}
 
@@ -47,5 +106,58 @@ public class PersistenciaSuperandes {
 		
 	     cerrarUnidadPersistencia();
 	}
+	
+	private LinkedList<String> leerNombreTablas(JsonObject tableConfig) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void crearClasesSQL()
+	{
+		sqlBodega = new SQLBodega(this);
+		sqlCategoriaProducto = new SQLCategoriaProducto(this);
+		sqlCliente = new SQLCliente(this);
+		sqlEstante = new SQLEstante(this);
+		sqlProducto = new SQLProducto(this);
+		sqlProveedor = new SQLProveedor(this);
+		sqlSucursal = new SQLSucursal(this);
+		sqlSupermercado = new SQLSupermercado(this);
+		sqlUtil = new SQLUtil(this);
+		
+		
+	}
 
+	public String darTablaBodega() {
+		return tablas.get(1);
+	}
+	
+	public String darTablaCliente() {
+		return tablas.get(2);
+	}
+	
+	public String darTablaEstante() {
+		return tablas.get(3);
+	}
+	
+	public String darTablaProducto() {
+		return tablas.get(4);
+	}
+	
+	public String darTablaCategoriaProducto() {
+		return tablas.get(5);
+	}
+	
+	public String darTablaProveedor() {
+		return tablas.get(6);
+	}
+	
+	public String darTablaSucursal() {
+		return tablas.get(7);
+	}
+	
+	public String darTablaSupermercado() {
+		return tablas.get(8);
+	}
+	
+	
 }
